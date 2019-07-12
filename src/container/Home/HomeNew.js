1052980bc1ad5bb1200addf7a9dfd0d1ea4f8d3d
colorlessenergy <brianmunozdev@gmail.com>
2019-07-11 17:19:50 -0700

    filter builds by champion name as per issue #36

--- src/container/Home/HomeNew.js
+++ src/container/Home/HomeNew.js
@@ -12,12 +12,60 @@ import SortOptions from './SortOptions'
  * display all the builds
  */
 class Home extends Component {
+  state = {
+    filteredBuilds: [],
+    champion: ''
+  }
+
+  /**
+   * if the input to filter champions is empty 
+   * display all the builds
+   * 
+   * @param {*} props - props which contains all the builds
+   * @param {*} state = state which contains the value of the input
+   */
+
+  static getDerivedStateFromProps(props, state) {
+    if (state.champion === '') {
+      return {
+        filteredBuilds: props.builds
+      }
+    }
+  }
+
+  /**
+   *  get the value of the input and sort the champions with that value
+   *  
+   *  @param {String} ev.target.value - the string that user inputs to filter champions
+   */
+
+  handleChange = (ev) => {
+
+    let filteredBuilds = this.props.builds.filter((build) => {
+      return build.champion.toLowerCase().includes(ev.target.value);
+    });
+
+    this.setState({
+      champion: ev.target.value,
+      filteredBuilds: filteredBuilds
+    })
+  }
 
   render() {
     return (
       <React.Fragment>
+        <div>
+          <label htmlFor="champion">
+            champion
+          </label>
+          <input type="text"
+            onChange={this.handleChange}
+            id="champion"
+            value={this.state.champion} />
+        </div>
+
         <SortOptions />
-        <BuildList url='/build/' builds={this.props.builds} />
+        <BuildList url='/build/' builds={this.state.filteredBuilds} />
       </React.Fragment>
     )
   }
